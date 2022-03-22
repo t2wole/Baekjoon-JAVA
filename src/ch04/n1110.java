0698f8c0d2dbe517f69ce20e2d6761bfc34af6e7
jushack <3.jushack.1@gmail.com>
2022-03-22 19:52:56 +0900

    first

---
+++ src/ch04/n1110.java
@@ -0,0 +1,28 @@
+package ch04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class n1110 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int count = 0;
+		int num = n;
+		
+		do {
+			n = ((n % 10) * 10) + (((n / 10) + (n % 10)) % 10);
+			count++;
+		} while(num != n);
+		
+		System.out.println(count);
+		
+		
+		
+	}
+
+}
